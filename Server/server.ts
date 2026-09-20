8a464c207d151ddbc896ef4c2de3aa269627108f
Ritesh <vishwakarmaritesh5585@gmail.com>
2026-09-20 13:51:49 +0530

    feat: deployment setup

--- Server/server.ts
+++ Server/server.ts
@@ -1,14 +1,24 @@
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-dotenv.config();
-
+import "dotenv/config";
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import app from "./src/app.js";
 import { connectDb } from "./src/config/db.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
 
 await connectDb();
 
+const clientPath = path.join(__dirname, "../../Client/dist");
+app.use(express.static(clientPath));
+
+app.get("*path", (req, res) => {
+  res.sendFile(path.join(clientPath, "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
--- Server/src/app.ts
+++ Server/src/app.ts
@@ -9,9 +9,9 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
